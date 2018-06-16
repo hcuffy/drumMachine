4f7b29fbd255235aae9d558b2e599f4df6cfa905
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-16 16:50:41 +0200

    Adding audio buttons

--- src/App/App.js
+++ src/App/App.js
@@ -8,28 +8,119 @@ class App extends Component {
    super(props);
 
    this.state = {
-
+    audioLabel : ''
    };
    this.drumAudioQ = this.drumAudioQ.bind(this);
+   this.q_Audio  = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+   this.drumAudioW = this.drumAudioW.bind(this);
+   this.w_Audio  = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+   this.drumAudioE = this.drumAudioE.bind(this);
+   this.e_Audio  = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+   this.drumAudioA = this.drumAudioA.bind(this);
+   this.a_Audio  = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+   this.drumAudioS = this.drumAudioS.bind(this);
+   this.s_Audio  = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+   this.drumAudioD = this.drumAudioD.bind(this);
+   this.d_Audio  = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+   this.drumAudioZ = this.drumAudioZ.bind(this);
+   this.z_Audio  = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+   this.drumAudioX = this.drumAudioX.bind(this);
+   this.x_Audio  = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+   this.drumAudioC = this.drumAudioC.bind(this);
+   this.c_Audio  = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
 }
 
- drumAudioQ(){
 
+  drumAudioQ() {
+    let { audioLabel } = this.state;
+    this.setState({
+      audioLabel: 'Q'
+    });
+    this.q_Audio.play();
+  }
 
+  drumAudioW() {
+    let { audioLabel } = this.state;
+    this.setState({
+      audioLabel: 'W'
+    });
+    this.w_Audio.play();
+  }
 
- }
+  drumAudioE() {
+    let { audioLabel } = this.state;
+    this.setState({
+      audioLabel: 'E'
+    });
+    this.e_Audio.play();
+  }
+  drumAudioA() {
+    let { audioLabel } = this.state;
+    this.setState({
+      audioLabel: 'A'
+    });
+    this.a_Audio.play();
+  }
+  drumAudioS() {
+    let { audioLabel } = this.state;
+    this.setState({
+      audioLabel: 'S'
+    });
+    this.s_Audio.play();
+  }
+  drumAudioD() {
+    let { audioLabel } = this.state;
+    this.setState({
+      audioLabel: 'D'
+    });
+    this.d_Audio.play();
+  }
+
+  drumAudioZ() {
+    let { audioLabel } = this.state;
+    this.setState({
+      audioLabel: 'Z'
+    });
+    this.z_Audio.play();
+  }
+
+  drumAudioX() {
+    let { audioLabel } = this.state;
+    this.setState({
+      audioLabel: 'X'
+    });
+    this.x_Audio.play();
+  }
+
+  drumAudioC() {
+    let { audioLabel } = this.state;
+    this.setState({
+      audioLabel: 'C'
+    });
+    this.c_Audio.play();
+  }
 
   render() {
     return (
       <div id="drum-machine" className="App">
- <header>
-<h1>Drum Machine</h1>
- </header>
+         <header>
+            <h1>Drum Machine</h1>
+         </header>
         <div id="display">
-
-        <Board
-         drumAudioQ={this.drumAudioQ}
-        />
+            <Board
+             drumAudioQ={this.drumAudioQ}
+             drumAudioW={this.drumAudioW}
+             drumAudioE={this.drumAudioE}
+             drumAudioA={this.drumAudioA}
+             drumAudioS={this.drumAudioS}
+             drumAudioD={this.drumAudioD}
+             drumAudioZ={this.drumAudioZ}
+             drumAudioX={this.drumAudioX}
+             drumAudioC={this.drumAudioC}
+            />
+            <div>
+            <label>{this.state.audioLabel}</label>
+            </div>
         </div>
       </div>
     );
--- src/App/Components/board.js
+++ src/App/Components/board.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../App.css';
+import './components.css';
 
 
 const Board = (props) => {
@@ -7,18 +7,18 @@ const Board = (props) => {
   return (<table className="table">
      <tr>
         <td id="drumQ" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioQ}>Q</button></td>
-        <td id="drumW" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioQ}>W</button></td>
-        <td id="drumE" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioQ}>E</button></td>
+        <td id="drumW" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioW}>W</button></td>
+        <td id="drumE" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioE}>E</button></td>
       </tr>
       <tr>
-        <td id="drumA" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioQ}>A</button></td>
-        <td id="drumS" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioQ}>S</button></td>
-        <td id="drumD" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioQ}>D</button></td>
+        <td id="drumA" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioA}>A</button></td>
+        <td id="drumS" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioS}>S</button></td>
+        <td id="drumD" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioD}>D</button></td>
       </tr>
       <tr>
-        <td id="drumZ" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioQ}>Z</button></td>
-        <td id="drumX" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioQ}>X</button></td>
-        <td id="drumC" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioQ}>C</button></td>
+        <td id="drumZ" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioZ}>Z</button></td>
+        <td id="drumX" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioX}>X</button></td>
+        <td id="drumC" class="drum-pad"><button type="button" class="btn btn-info" onClick={props.drumAudioC}>C</button></td>
       </tr>
 
   </table>);
